70e59e61321d8edf0c733d425363174a382a636f
alireza <alirezanghngh1379@gmail.com>
2024-03-04 13:51:13 +0330

    initial-data

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import './index.css'
+
+function App() {
+  return (
+<Routes>
+  <Route path='/' element={<di >auth page</di>}/>
+</Routes>
+  )
+}
+
+export default App
