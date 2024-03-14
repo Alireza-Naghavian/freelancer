cb951f468f75fa51989e737e0ed09ed58f9aac36
alireza <alirezanghngh1379@gmail.com>
2024-03-14 17:38:56 +0330

    fix-minor-bug

--- src/services/proposalServices.js
+++ src/services/proposalServices.js
@@ -1,12 +1,12 @@
 import http from "./httpservices";
 
-export const chageProposalStApi = ({ id, data }) => {
-  return http.patch(`/proposal/${id}`, data).then(({ data }) => data.data);
+export const chageProposalStApi = ({ proposalId, ...data }) => {
+  return http.patch(`/proposal/${proposalId}`, data).then(({ data }) => data.data);
 };
 
 export const getProposalsApi = () => {
   return http.get(`/proposal/list`).then(({ data }) => data.data);
 };
-export const sendProposalApi = ( data ) => {
+export const sendProposalApi = ( data) => {
   return http.post(`/proposal/add`, data).then(({ data }) => data.data);
 };
